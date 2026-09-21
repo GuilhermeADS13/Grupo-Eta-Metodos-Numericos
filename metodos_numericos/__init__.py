1d16d6362284d6409ecc8844806087c571341d02
Guilherme Maciel <lakilleo@outlook.com>
2026-09-21 17:06:09 -0300

    chore: ajustes no notebook e no __init__ do pacote

--- metodos_numericos/__init__.py
+++ metodos_numericos/__init__.py
@@ -1,8 +1,3 @@
-"""Metodos numericos para determinacao de raizes de equacoes nao-lineares.
-
-Implementado pelo Grupo Eta para a disciplina de Metodos Numericos.
-Os tres metodos sao genericos: recebem a funcao do problema por parametro.
-"""
 
 from .bissecao import bissecao
 from .newton_raphson import newton_raphson
