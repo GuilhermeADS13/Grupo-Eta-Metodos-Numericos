5b8208fb14abf12a960ee3988b44a858c8ef8cba
Guilherme Maciel <lakilleo@outlook.com>
2026-09-21 16:15:31 -0300

    feat: Organizar o main para deixar mais visivel

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from bissecao import bissecao
 from newton_raphson import newton_raphson
 from  ponto_fixo import ponto_fixo
 
-
+print("Método da Bisseção:")
 raiz, iteracoes = bissecao(f, 1, 2, eps1, eps2, kmax)
-print("Bissecao: i =", raiz, "em", iteracoes, "iteracoes")
-
+print("Bissecao: i =", raiz, "em", iteracoes, "iteracoes" "\n")
+print("Método de Newton-Raphson:" "\n")
 raiz, iteracoes = newton_raphson(f, f_linha, 1, eps1, eps2, kmax)
-print("Newton-Raphson: i =", raiz, "em", iteracoes, "iteracoes")
-
+print("Newton-Raphson: i =", raiz, "em", iteracoes, "iteracoes" "\n")
+print("Método do Ponto Fixo:" "\n")
 raiz, iteracoes = ponto_fixo(phi, f, 1, eps1, eps2, kmax)
-print("Ponto Fixo: i =", raiz, "em", iteracoes, "iteracoes")
+print("Ponto Fixo: i =", raiz, "em", iteracoes, "iteracoes" "\n")
